bd32f9865594bedea540730ae669f1177199ce09
Sobilo34 <bilalsolih60@gmail.com>
2024-02-27 10:12:29 -0800

    A function that measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,14 +10,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_edge, right_edge;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0); /* Return 0 if tree is NULL*/
 
 	/* Calculate the height of left and right edges recursively*/
 	left_edge = binary_tree_height(tree->left);
 	right_edge = binary_tree_height(tree->right);
 
-	if (left_edge > right_edge)
+	if (left_edge < right_edge)
 	{
 		return (1 + left_edge); /*height of left edge*/
 	}
