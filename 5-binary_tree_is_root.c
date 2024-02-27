1b8c63f5332379697629e24759f9800c9afc77bb
Sobilo34 <bilalsolih60@gmail.com>
2024-02-27 08:47:53 -0800

    A dfunction tha checks if a node is a root node

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - A function that checked if node is a leaf node
+ * binary_tree_is_root - A function that checks if a given node is a root
  * @node: The pointer to the node to check
- * Return: The 1 if the node is a leaf node and 0 if not
+ * Return: The 1 if the node a a root node and 0 if not
  */
 
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (!node)
+	if (node == NULL)
 		return (0); /* Return 0 if node is NULL*/
 
-	if (node->right == NULL && node->left == NULL)
+	if (node != NULL && !node->parent)
 	{
-		return (1); /* Return 1 if its a leaf node*/
+		return (1); /* Return 1 if there's a node and not parent node*/
 	}
 
 	return (0); /* Return 0 if otherwise */
